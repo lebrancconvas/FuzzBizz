e5a0cea95138751b176777e6300396205ddfc487
lebrancconvas <lebrancconvas@gmail.com>
2024-06-29 01:34:00 +0700

    add pass tests

--- main_test.go
+++ main_test.go
@@ -4,12 +4,24 @@ import (
 	"testing"
 )
 
-func TestFuzzBizz(t *testing.T) {
-	got := FuzzBizz(1)
-
-	want := "FuzzBizz"
-
-	if got != want {
-		t.Errorf("1 should return FuzzBizz")
-	} 
+type TestCase struct {
+	name string
+	input int
+	expect string
 }
+
+func TestFuzzBizz(t *testing.T) {
+	testCases := []TestCase{
+		{"1 should return FuzzBizz", 1, "FuzzBizz"},
+		{"2 should return FuzzBizz", 2, "FuzzBizz"},
+	}
+
+	for _, tt := range testCases {
+		got := FuzzBizz(tt.input)
+
+		if got != tt.expect {
+			t.Errorf("%v should return %v, but got %v", tt.input, tt.expect, got)
+		}
+	}
+}
+
